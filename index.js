37247b7e73d958d58aa6dc3d79f88760239a59e4
sondos alansi <sondos.alansi@yahoo.com>
2018-09-06 21:37:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(employee){
+  for (let i=0; i<employee.length; i++){
+    console.log(`Welcome ${employee[i]}! You are employee #${i+1}.`);
+  }
+  return employee;
+  
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){
+  let tails=0;
+  while(maybeTrue()){
+   
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
